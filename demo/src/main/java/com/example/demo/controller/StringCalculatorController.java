c89f9513651d559cd177dd82e7b328a580c33a19
Hasti711 <81065916+Hasti711@users.noreply.github.com>
2024-10-27 20:11:59 +0530

    Add files via upload

--- demo/src/main/java/com/example/demo/controller/StringCalculatorController.java
+++ demo/src/main/java/com/example/demo/controller/StringCalculatorController.java
@@ -1,1 +1,25 @@
+package com.example.demo.controller;
 
+import com.example.demo.service.StringCalculatorService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/calculator")
+public class StringCalculatorController {
+
+    private final StringCalculatorService service;
+
+    public StringCalculatorController(StringCalculatorService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/add")
+    public int add(@RequestBody String numbers) throws Exception {
+        return service.add(numbers);
+    }
+
+    @GetMapping("/count")
+    public int getCallCount() {
+        return service.getCallCount();
+    }
+}
