6a3a894e94f7932e1e1a7575ea3c6c3aea2e9924
Hasti711 <81065916+Hasti711@users.noreply.github.com>
2024-10-27 20:14:27 +0530

    Add files via upload

--- demo/src/main/java/com/example/demo/repositroy/StringCalculatorRepository.java
+++ demo/src/main/java/com/example/demo/repositroy/StringCalculatorRepository.java
@@ -1,1 +1,20 @@
+package com.example.demo.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.demo.domain.Calculation;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StringCalculatorRepository {
+    private final List<Calculation> calculations = new ArrayList<>();
+
+    public void save(Calculation calculation) {
+        calculations.add(calculation);
+    }
+
+    public List<Calculation> findAll() {
+        return calculations;
+    }
+}
