d718711a583562f383cfcad473f616ce7c87aec4
Hasti711 <81065916+Hasti711@users.noreply.github.com>
2024-10-27 20:15:34 +0530

    Add files via upload

--- demo/src/main/java/com/example/demo/domain/Calculation.java
+++ demo/src/main/java/com/example/demo/domain/Calculation.java
@@ -1,1 +1,13 @@
+package com.example.demo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Calculation {
+    private String input;
+    private int result;
+}
